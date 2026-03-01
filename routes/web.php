06d51526a96fa880ac61a31774d6c8aa80850aee
jdolan182 <jordandolan182@gmail.com>
2026-03-01 19:38:24 +0000

    inital commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TestController; 
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test', [TestController::class, 'test']);
